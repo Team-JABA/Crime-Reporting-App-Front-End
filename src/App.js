b939a32477693408215703536d28a699242c9d5c
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-12-21 12:15:40 -0800

    brought in MUI/Sample header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from './modules/Header/Header';
+
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<header className='App-header'>
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
