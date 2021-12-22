e49316be4663fde240de0b563b7a312dc17f1fd4
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-12-22 14:26:18 -0800

    developer map embed rendering

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from './modules/Map/Map';
+
+export default function Main() {
+	return (
+		<>
+			<Map />
+		</>
+	);
+}
---
+++ src/modules/Map/Map.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+function Map() {
+	const MAP_API = process.env.REACT_APP_MAP_API_KEY;
+
+	const containerStyle = {
+		width: '700px',
+		height: '450px',
+	};
+
+	const center = {
+		lat: 32.778318544903414,
+		lng: -117.22823187488879,
+	};
+
+	return (
+		<LoadScript googleMapsApiKey={MAP_API}>
+			<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+				{/* Child components, such as markers, info windows, etc. */}
+				<></>
+			</GoogleMap>
+		</LoadScript>
+	);
+}
+
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default Map;
+
+// 32.778318544903414, -117.22823187488879
