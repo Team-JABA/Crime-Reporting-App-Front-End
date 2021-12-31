cec917312980750dac37d271a73b74380b6b6fa3
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-12-31 15:09:12 -0800

    fixed Auth0 login bug

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import MapProvider from './context/map';
-import Auth0ProviderWithHistory from './context/auth0-provider-with-history';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
 	<Router>
-		<Auth0ProviderWithHistory>
+		<Auth0Provider
+		  domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}>
 			<MapProvider>
 				<App />
 			</MapProvider>
-		</Auth0ProviderWithHistory>
+		</Auth0Provider>
 	</Router>,
 	document.getElementById('root'),
 );
